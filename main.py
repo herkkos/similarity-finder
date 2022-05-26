71d4644ea0199f82f0ac455fb4e7f1be3c353ec8
Herkko Salonen <herkko.salonen@hotmail.fi>
2022-05-27 00:58:32 +0300

    Overall work. Change similarity formula

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 27 02:41:45 2021
-
-@author: Herkko
+Basic command line interface for using image similarity finder
 """
 
-# Basic command line interface for using image similarity finder
 
 import argparse
 from spatial_similarity import SpatialSimilarity
+from config import ConfigManager
+from data import DataManager
 
-parser = argparse.ArgumentParser(description='Welcome to similarity finder!')
-parser.add_argument('folder', type=ascii, nargs=1, help='Source folder of images')
-parser.add_argument('output file', type=ascii, nargs=1, help='Destination for output file')
-parser.add_argument('-t', '--type', type=int, nargs=1, help='Type of similarity search')
-parser.add_argument('-s', '--threshold', type=float, help='Threshold for similarity')
-parser.add_argument('-f', '--filetypes', type=ascii, help='Filetypes to scan')
-THRESHOLD = None
-FILETYPES = None
 
-args = parser.parse_args()
-if args.threshold:
-    THRESHOLD = args.threshold
-if args.filetypes:
-    FILETYPES = args.filetypes
-if args.folder and args.output:
-    if args.type:
-        if args.type == 'spatial':
-            # Call for spatial similarity
-            print('Calling spatial similarity')
-            SpatialSimilarity(args.folder, args.output, THRESHOLD, FILETYPES)
-        elif args.type == 'freq':
-            # Call for frequence based similarity
-            print('TODO')
+def parse_args():
+    parser = argparse.ArgumentParser(description='Welcome to similarity finder!')
+    parser.add_argument('folder', type=ascii, nargs=1, help='Source folder of images')
+    parser.add_argument('output file', type=ascii, nargs=1, help='Destination for output file')
+    parser.add_argument('-t', '--type', type=int, nargs=1, help='Type of similarity search')
+    parser.add_argument('-s', '--threshold', type=float, help='Threshold for similarity')
+    parser.add_argument('-f', '--filetypes', type=ascii, help='Filetypes to scan')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    
+    config = ConfigManager()
+    data = DataManager(config)
+
+    if args.threshold:
+        config.set_threshold(args.threshold)
+    if args.filetypes:
+        config.set_filetypes(args.filetypes)
+    if args.folder:
+        config.set_input_paths(args.folder)
+    if args.output:
+        config.set_output_path(args.output)
+        
+    if args.type and args.type == 'freq':
+        print('TODO')
     else:
-        # Call for spatial similarity
         print('Calling spatial similarity')
-        SpatialSimilarity(args.folder, args.output, THRESHOLD, FILETYPES)
+        SpatialSimilarity(config, data)
+
+
+if __name__ == '__main__':
+    main()
+
--- spatial_similarity.py
+++ spatial_similarity.py
@@ -1,190 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 13 12:47:42 2020
-
-@author: Herkko
 """
 
-import os
-import json
+import concurrent.futures
 import itertools
 import cv2
-import imagehash
+import multiprocessing
 import numpy as np
+from typing  import List
+from config import ConfigManager
+from data import DataManager
 
+MAX_RES = 24
 
 class SpatialSimilarity:
-    '''
-    Class to find spatial similarity within images.
 
-    ...
-    Attributes
-    ----------
-    filetypes : string array
-        filetypes to be scanned
-    folder : string
-        path to folder which contains images
-    output : string
-        path to output file
-    threshold : float
-        threshold for similarity, 0 being absolute copy
+    def __init__(self, config : ConfigManager, data : DataManager):
+        image_files = data.get_filepaths()
+        image_array = scan(image_files)
+        similar_ones = self.find_similarity(image_array, config.get_threshold())
+        data.set_similars(similar_ones)
+        data.write_to_json()
 
-    Methods
-    -------
-    findSimilarity(files):
-        returns array of similar images
-    getFiles(folders):
-        returns paths to files from folders and subfolders
-    '''
-
-    def __init__(self, folder, output, threshold=0.0,
-                 filetypes=['.jpg', '.JPG', '.png', '.PNG', 'jpeg', 'JPEG']):
-        '''
-        Constructs class for finding similarity between images.
-
-        Parameters
-        ----------
-        folder : string
-            Path to folder which containts images.
-        output : string
-            Path to output file.
-        threshold : float
-            Threshold for image similarity. The default is 0.0.
-        filetypes : string array
-            Filetypes to be scanned.
-
-        '''
-        self.folder = folder
-        self.output = output
-        self.threshold = threshold
-        self.filetypes = filetypes
-
-        image_files = self.get_files(self.folder)
-        image_list = scan(image_files)
-        similar_ones = self.find_similarity(image_list)
-        write_to_json(similar_ones, self.output)
-
-    def find_similarity(self, images):
-        '''
-        Finds similar images from array of imagehashes.
-
-        Parameters
-        ----------
-        images : string array
-            array of imagehashes and respective filepaths
-
-        Returns
-        -------
-        array string
-            1-dim is image instance
-            2-dim is all files of same instance
-
-        '''
-        similar_ones = np.array([])
-        for first_image, second_image in itertools.combinations(images, 2):
-            similarity = first_image[1] - second_image[1]
-            # TODO: implement some kind of normalization for similarity
-            if similarity <= self.threshold:
-                similar_ones = np.append(
-                    similar_ones, (first_image[0], second_image[0]))
-        return similar_ones.reshape(int(similar_ones.size / 2), 2)
-
-    def get_files(self, folders):
-        '''
-        Loads all filenames into an array
-
-        Parameters
-        ----------
-        folders : string array
-            Array of paths to folders.
-
-        Returns
-        -------
-        path_array : array string
-            Array of paths to files.
-
-        '''
-        path_array = []
-        for folder in folders:
-            for root, dirs, files, in os.walk(folder):
-                for name in files:
-                    # TODO: better way to determine filetype
-                    if name[-4:] in self.filetypes:
-                        path_array.append(os.path.join(root, name))
-        return path_array
-
+def find_similarity(self, image_array, threshold):
+    similar_ones = np.array([])
+    for first_image, second_image in itertools.combinations(image_array, 2):
+        #TODO: more sophisticated way to calculate image similarity
+        # Should be noted that this already implements pyramidic grid though
+        # it could be imporved by changing the order of grid elements
+        similarity = first_image[1].astype('int32') - second_image[1].astype('int32')
+        similarity = np.abs(similarity.mean())
+        if similarity <= threshold:
+            similar_ones = np.append(
+                similar_ones, (first_image[0], second_image[0]))
+    return similar_ones.reshape(int(similar_ones.size / 2), 2)
 
 def load_file(file):
-    '''
-    Loads file and returns a hash which summarizes the image contents.
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        return np.array([])
+    pyramid_data = []
+    for i in range(1, MAX_RES+1):
+        clone_img = img.copy()
+        clone_img = cv2.resize(clone_img, (i,i))
+        for a in clone_img.reshape(clone_img.size):
+            pyramid_data.append(a) 
+    return np.array(pyramid_data)
 
-    Parameters
-    ----------
-    file : string
-        path to file
-
-    Returns
-    -------
-    imhash : hash/string
-        imagehash calculated from file
-
-    '''
-    image = cv2.imread(file, 0)
-    image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_LINEAR)
-    image = imagehash.Image.fromarray(image)
-    imhash = imagehash.dhash(image, 32)
-    return imhash
-
-
-def scan(files):
-    '''
-    Returns a 2D numpy array of tuples containing imagehash and filepath
-
-    Parameters
-    ----------
-    files : string array
-        paths to files
-
-    Returns
-    -------
-    image_array : string tuples
-        tuples containing imagehash and filepaths
-
-    '''
-    image_array = np.array([])
-    for file in files:
-        image_array = np.arrend(image_array, [file, load_file(file)])
-    image_array = image_array.reshape(int(image_array.size / 2), 2)
-    return image_array
-
-
-def write_to_json(array, output):
-    '''
-    Writes similarities in output json file array contains numpy list of
-    combinations of similar images
-
-    Parameters
-    ----------
-    array : string
-        Contains paths to similar images.
-    OUTPUT : string
-        Path to output file.
-
-    Returns
-    -------
-    None.
-
-    '''
-    # TODO: smart implementation to go through all containing values
-    data = {}
-    for pair in array:
-        if pair[0] in data:
-            data[pair[0]].append(pair[1])
-        elif pair[1] in data:
-            data[pair[1]].append(pair[0])
-        else:
-            data[pair[0]] = []
-            data[pair[0]].append(pair[1])
-
-    with open(output, 'w') as outfile:
-        json.dump(data, outfile)
+def scan(files : List):
+    image_array = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() - 1) as executor:
+        futures = {executor.submit(load_file, file): file for file in files}
+        for future in concurrent.futures.as_completed(futures):
+            file = futures[future]
+            data = future.result()
+            if data.size == 0:
+                continue
+            image_array.append((file, data))
+        
+    return np.array(image_array)
